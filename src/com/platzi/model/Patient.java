2fe708a8685c4e6e7e1c3a1f6f01815ba61e45a6
Fernando Ambrosio <fer22ay@gmail.com>
2019-12-31 10:40:30 -0600

    Clase Paciente v.0.1

---
+++ src/com/platzi/model/Patient.java
@@ -0,0 +1,54 @@
+package com.platzi.model;
+
+import com.platzi.users.User;
+
+public class Patient extends User {
+
+    private String birthday;
+    private double weight;
+    private double height;
+    private String blood;
+
+    public Patient(String name, String email) {
+        super(name, email);
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getWeight() {
+        return weight + " Kg.";
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public String getHeight() {
+        return height + " Mts.";
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+
+    @Override
+    public void showDataUser() {
+        System.out.println("Paciente");
+        System.out.println("Historial completo del paciente desde nacimiento");
+    }
+
+}
