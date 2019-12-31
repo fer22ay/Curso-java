11a517cf808ba86f6e680c67e6271f1eaa028e9a
Fernando Ambrosio <fer22ay@gmail.com>
2019-12-31 15:08:55 -0600

    Clase Main para correr la aplicación.

---
+++ src/com/platzi/clases/Main.java
@@ -0,0 +1,15 @@
+package com.platzi.clases;
+
+import com.platzi.model.Doctor;
+import com.platzi.model.Patient;
+import static com.platzi.menu.UIMenu.*;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        showMenu();
+       
+    }
+}
