032eb6bb61ce798d59386bdaf68f14bf51a60d79
Fernando Ambrosio <fer22ay@gmail.com>
2019-12-31 19:48:06 -0600

    Creando lista de objetos de las clases AppointmentDoctor y Nurse

--- src/com/platzi/model/Patient.java
+++ src/com/platzi/model/Patient.java
@@ -1,6 +1,8 @@
 package com.platzi.model;
 
 import com.platzi.users.User;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Patient extends User {
 
@@ -8,6 +10,27 @@ public class Patient extends User {
     private double weight;
     private double height;
     private String blood;
+    
+    private ArrayList<AppointmentDoctor> appointmentDoctors = new ArrayList<>();
+    private ArrayList<AppointmentNurse> appointmentNurses = new ArrayList<>();
+
+    public ArrayList<AppointmentDoctor> getAppointmentDoctors() {
+        return appointmentDoctors;
+    }
+
+    public void addAppointmentDoctors(Doctor doctor, Date date, String time) {
+        AppointmentDoctor appointmentDoctor = new AppointmentDoctor(this, doctor);
+        appointmentDoctor.obtenerCita(date, time);
+        appointmentDoctors.add(appointmentDoctor);
+    }
+
+    public ArrayList<AppointmentNurse> getAppointmentNurses() {
+        return appointmentNurses;
+    }
+
+    public void setAppointmentNurses(ArrayList<AppointmentNurse> appointmentNurses) {
+        this.appointmentNurses = appointmentNurses;
+    }
 
     public Patient(String name, String email) {
         super(name, email);
